5feee77e4e1d48ffcac0aefc1501a7cef8ee122b
Kevin <k.urbain.personnel@gmail.com>
2026-04-09 19:08:47 +0200

    deleted : logErrorException added : PHPDoc for functions of auth.php

--- auth.php
+++ auth.php
@@ -1,25 +1,37 @@
 <?php
-//A UTILISER POUR PROTEGER DES PAGES CONTRE LA MANIPULATION DES URL SANS ETRE CONNECTES.
-function check_login()
+declare(strict_types=1);
+
+/**
+ * Vérification de l'existence d'une session $_SESSION['user']
+ * 
+ * Il faut préciser l'URL pour rediriger l'utilisateur. L'utilisation de constantes propres est recommandé.
+ * @return void
+ */
+function check_login($url = ''): void
 {
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
 
     if (!isset($_SESSION['user'])) {
-        header('Location: ' . BASE_URL . '/login');
+        header('Location: ' . $url . '/login');
         exit();
     }
 }
 
-function check_admin()
+/**
+ * Vérification de l'existence d'une session $_SESSION['user']['role'] et quelle est égale à 'admin'
+ * 
+ * Il faut préciser l'URL pour rediriger l'utilisateur. L'utilisation de constantes propres est recommandé.
+ * Cette fonction est dépendante de check_login()
+ * @return void
+ */
+function check_admin($url = ''): void
 {
-
     check_login();
 
-
     if (!isset($_SESSION['user']['role']) || $_SESSION['user']['role'] !== 'admin') {
-        header('Location: ' . BASE_URL . '/home');
+        header('Location: ' . $url . '/home');
         exit();
     }
 }
--- logErrorException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function log_errorException(Throwable $e, $logFile = ROOT . '/log/error_log.txt')
-{
-    $message = date('Y-m-d H:i:s') . ' : ' . $e->getMessage() . ', errorCode=' . $e->getCode() . "\n";
-    error_log($message, 3, $logFile);
-}
-
