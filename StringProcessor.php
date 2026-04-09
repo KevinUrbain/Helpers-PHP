66639d04b767c5a66bbbecf6d75ac62bf28c9bc9
Kevin <k.urbain.personnel@gmail.com>
2026-04-09 19:15:04 +0200

    added : contraint if $text  length === 0

--- StringProcessor.php
+++ StringProcessor.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 /**
  * Traite une chaîne de caractères pour compter les occurrences de mots.
  *
+ * Si le texte est vide, un tableau vide est retourné
  * @param string $text Le texte brut.
- * @return array<string, int> Tableau [mot => nombre d'occurrences], trié par fréquence.
+ * @return array Tableau [mot => nombre d'occurrences], trié par fréquence.
  */
 function stringProcessor(string $text): array
 {
+
+    if (strlen($text) === 0) {
+        return [];
+    }
+
     $normalizedText = mb_strtolower($text);
 
     $cleanText = str_replace(['.', ',', ';', '!', '?', '"', "'", '-'], ' ', $normalizedText);
@@ -24,8 +30,9 @@ function stringProcessor(string $text): array
     return $frequencies;
 }
 
+//Utilisation
+
 $text = "Accordez-moi, je vous offre mon bras. Rudement, un grand et long cri suprême de désespoir. Appuyons ceci d'un indice quelconque. Rivaux, après que je les sentais ; j'étudiais encore, je regardais tomber la pluie et l'obscurité, jusqu'à leur mort, quand une poussière enflammée tourbillonnera dans nos rues. Cinglés par la pluie, ne regardant rien qu'eux-mêmes. Traité dès lors comme un scélérat. Malheureux qu'elle a songé à vous aider. Bonne et hospitalière d'ailleurs, rappeler sur mon instrument, la voix tonnante du lion et les cris de quelque chauve-souris qu'un chat-huant avait surprise.";
 
 $tableau = stringProcessor($text);
-
 var_dump($tableau);
